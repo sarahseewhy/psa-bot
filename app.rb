a190981a8cc85c88f486af4abaa27138dc738c07
Sarah <sarahseewhy@gmail.com>
2017-11-01 15:12:13 +0000

    Adds postback definition and logic

--- app.rb
+++ app.rb
@@ -6,3 +6,8 @@ get '/psa' do
   postback params[:text], params[:channel_id]
   status 200
 end
+
+def postback message, channel
+    slack_webhook = ENV['SLACK_WEBHOOK_URL']
+    HTTParty.post slack_webhook, body: {"text" => message.to_s, "username" => "PSA", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
+end
